5a75666d6bb979a6db4b9bc8268de06e5564a17d
Eric Papain <36469392+ericpapain@users.noreply.github.com>
2019-04-03 09:52:50 +0700

    Add files via upload

---
+++ test/java/org/projetGL/init/GestionRendezVousApplicationTests.java
@@ -0,0 +1,17 @@
+package org.projetGL.init;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionRendezVousApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
